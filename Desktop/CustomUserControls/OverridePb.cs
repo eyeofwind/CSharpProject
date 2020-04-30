28ac371ba2f20a4b0a773e9863d918652c45d1ba
Kurris <791444095@qq.com>
2020-04-30 09:26:11 +0800

    add a new customcontrol of progress,using to change progress color

---
+++ Desktop/CustomUserControls/OverridePb.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverrideProgressBar
+{
+    [Browsable(false)]
+    public class OverridePb : ProgressBar
+    {
+        public OverridePb()
+        {
+            base.SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //calc rectangle szie
+            Rectangle bounds = new Rectangle(0, 0, base.Width, base.Height);
+            bounds.Height -= 4;
+            bounds.Width = ((int)(bounds.Width * (((double)base.Value) / ((double)base.Maximum)))) - 4;
+
+            //Override color
+            e.Graphics.FillRectangle(new SolidBrush(Color.Orange), 2, 2, bounds.Width, bounds.Height);
+        }
+    }
+
+    /*How To Use
+        OverridePb bar = new OverridePb();
+            bar.Parent = Progress; //ProgressBar Control
+            bar.Minimum = 0;
+            bar.Maximum = 100;
+            bar.Width = Progress.Width;
+            bar.Height = Progress.Height;
+            bar.BackColor = Color.Gray;
+     */
+}
